21f30cb9b0e8137d8b0e9552709084be82dad711
aparnell0130 <70460766+aparnell0130@users.noreply.github.com>
2021-01-01 11:55:57 -0800

    moved asciiart-logo to its own folder for better readability

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 const dbFunctions = require('./db');
 const jsFunctions = require('./js');
-const logo = require('asciiart-logo');
-const config = require('./package.json');
+const logo = require('./assets/logo/logo')
 const questions = require('./js/questions')
 const inquirer = require('inquirer');
-console.log(
-    logo({
-        name: 'Employee Management System',
-        font: 'ANSI Shadow',
-        lineChars: 10,
-        padding: 2,
-        margin: 3,
-        borderColor: 'magenta',
-        logoColor: 'bold-cyan',
-        textColor: 'yellow',
-    })
-        .emptyLine()
-        .right('version 1.0')
-        .emptyLine()
-        .center('A simple way to manage your employees!')
-        .render()
-);
 startManagement = () => {
     inquirer.prompt(questions.options).then((results) => {
         switch (results.options) {
